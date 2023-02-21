99bff55deff93b73e5b6846ad9f35bfe51c1604c
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-02-21 17:40:59 +0000

    Add useEffect to change background color every 30 seconds

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "./App.css"
 import "./index.css"
+
+
 
 import albumData from "./database/albums"
 
@@ -8,9 +10,33 @@ import { AlbumForm, AlbumList, Bio, Header } from './components/index'
 
 
 function App() {
-  const [albums, setAlbums] = useState(albumData)
+    const [albums, setAlbums] = useState(albumData)
+    const [count, setCount] = useState(0)
 
-  return (
+    function getRandomColor() {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
+
+    useEffect(() => {
+
+        const randomColor = getRandomColor()
+        document.querySelector(".main-section").style.backgroundColor = randomColor
+
+        const timer = setInterval(() => {
+            setCount(prev => prev + 1)
+
+        }, 30000)
+
+        return () => {
+            clearInterval(timer)
+        }
+
+    }, [count])
+
+    return (
     <>
         <Header />
         <section className="main-section">
