2e18515337593d6e6099e4700dd00891370860ce
Richard Swainson <rich_swainson@hotmail.co.uk>
2023-02-22 12:22:18 +0000

    Add gradient background change every 30 seconds, add pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,19 @@ import { useState, useEffect } from 'react'
 import "./App.css"
 import "./index.css"
 
-
-
+import getRandomColor from "./helpers/helperFunctions"
 import albumData from "./database/albums"
-
 import { AlbumForm, AlbumList, Bio, Header } from './components/index'
 
 
 function App() {
     const [albums, setAlbums] = useState(albumData)
     const [count, setCount] = useState(0)
-
-    function getRandomColor() {
-        const red = Math.floor(Math.random() * 256);
-        const green = Math.floor(Math.random() * 256);
-        const blue = Math.floor(Math.random() * 256);
-        return `rgb(${red}, ${green}, ${blue})`;
-    }
+    const [bgColorArray, setBgColorArray] = useState(["#f6d365", "#fda085"])
 
     useEffect(() => {
 
-        const randomColor = getRandomColor()
-        document.querySelector(".main-section").style.backgroundColor = randomColor
+        setBgColorArray([getRandomColor(), getRandomColor()])
 
         const timer = setInterval(() => {
             setCount(prev => prev + 1)
@@ -39,7 +30,7 @@ function App() {
     return (
     <>
         <Header />
-        <section className="main-section">
+        <section className="main-section" style={{backgroundImage: `linear-gradient(120deg, ${bgColorArray[0]} 0%, ${bgColorArray[1]} 100%)`}}>
             <div className="section"><Bio /></div>
             <div className="section">
                 <AlbumForm 
